3ca1f18b55e095b91c10e5b06dc8031e56e8b50b
Asfand Yar <asfi.official@gmail.com>
2019-11-26 17:40:59 +0500

    Updates

---
+++ precache-manifest.d9bbefd54222013fbc739b9a508ee352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7495c59615761bfc6f79f5bf9d6f3e",
+    "url": "/crud-hooks/index.html"
+  },
+  {
+    "revision": "86918f0782a64dfc0a68",
+    "url": "/crud-hooks/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "d9abd22139ecfd88e187",
+    "url": "/crud-hooks/static/css/main.4ab936ed.chunk.css"
+  },
+  {
+    "revision": "86918f0782a64dfc0a68",
+    "url": "/crud-hooks/static/js/2.d83e9a74.chunk.js"
+  },
+  {
+    "revision": "d9abd22139ecfd88e187",
+    "url": "/crud-hooks/static/js/main.c2358cf1.chunk.js"
+  },
+  {
+    "revision": "81ce90f65eee6daab273",
+    "url": "/crud-hooks/static/js/runtime-main.5d1782b9.js"
+  }
+]);
